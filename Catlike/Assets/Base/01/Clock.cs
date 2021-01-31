fa7f062c64b9c6b811a0e30fb8d2e4f863547ba7
阿卡林 <1459439786@qq.com>
2021-01-31 23:59:39 +0800

    Pool,Event

---
+++ Catlike/Assets/Base/01/Clock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Base01
+{
+    public class Clock : MonoBehaviour
+    {
+        public Transform HoursArm;
+        public Transform MinutesArm;
+        public Transform SecondsArm;
+
+
+        const float degreesPerHour = 30f;//360度/60/60/24
+        const float degreesPerMinutes = 6f;//360度/60/60
+        const float degreesPerSeconds = 6f;//360度/60
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            DateTime time = DateTime.Now;
+            HoursArm.localRotation = Quaternion.Euler(0f, (float)time.Hour * degreesPerHour, 0f);
+            MinutesArm.localRotation = Quaternion.Euler(0f, (float)time.Minute * degreesPerMinutes, 0f);
+            SecondsArm.localRotation = Quaternion.Euler(0f, (float)time.Second * degreesPerSeconds, 0f);
+        }
+    }
+}
---
+++ Catlike/Assets/Base/02/Graph.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Base02
+{
+    public class Graph : MonoBehaviour
+    {
+        public Transform pointPrefab;
+
+        [SerializeField, Range(10, 100)]
+        int resolution = 10;
+
+        Transform[] points;
+
+        private void Awake()
+        {
+            float step = 2f / resolution;
+            Vector3 scale = Vector3.one * step;
+            Vector3 position;
+            position.z = 0f;
+
+            points = new Transform[resolution];
+            for (int i = 0; i < resolution; i++)
+            {
+                Transform point = Instantiate(pointPrefab);
+                position.x = (i + 0.5f) * step - 1f;
+                position.y = Mathf.Sin(position.x);
+                point.localPosition = position;
+                point.localScale = scale;
+                point.SetParent(transform);
+
+                points[i] = point;
+            }
+
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < points.Length; i++) 
+            {
+                Transform point = points[i];
+                Vector3 position = point.localPosition;
+                position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+                point.localPosition = position;
+            }
+        }
+    }
+}
